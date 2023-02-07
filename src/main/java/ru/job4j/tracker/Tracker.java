00b0b5445ed5e1bd992e2cfda0b087f30d387a02
ADIKIAA <zhekov.nick@yandex.ru>
2023-02-07 09:21:56 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #388930]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,7 +57,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
-            item.setId(items[index].getId());
+            item.setId(id);
             items[index] = item;
         } else {
             System.out.println("Заявка с таким id не найдена");
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.assertj.core.api.Assertions.assertThat;
-
 public class TrackerTest {
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() {
