7f83c29c9411057cfb039d8e5c8d8acd6c3a280c
ADIKIAA <zhekov.nick@yandex.ru>
2023-02-18 18:30:05 +0300

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,42 @@
+package ru.job4j.tracker;
+
+public final class SingleTracker {
+
+    private Tracker tracker = new Tracker();
+
+    private static SingleTracker instance = null;
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findByName(String name) {
+        return tracker.findByName(name);
+    }
+
+    public boolean deleteItem(int id) {
+        return tracker.delete(id);
+    }
+
+    public boolean editItem(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public Item[] showAll() {
+        return tracker.findAll();
+    }
+}
