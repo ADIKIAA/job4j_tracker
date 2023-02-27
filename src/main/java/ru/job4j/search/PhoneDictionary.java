c728107e11267b1fc0adaa75ab65f0490a63c81d
ADIKIAA <zhekov.nick@yandex.ru>
2023-02-28 01:10:48 +0300

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,23 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+
+    private ArrayList<Person> person = new ArrayList<>();
+
+    public void add(Person person) {
+        this.person.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person p : person) {
+            if (p.getName().contains(key) || p.getSurname().contains(key)
+                    || p.getPhone().contains(key) ||  p.getAddress().contains(key)) {
+                result.add(p);
+            }
+        }
+        return result;
+    }
+}
