b05eb5cd97af77711c3fc6080d9455929d62d234
ADIKIAA <zhekov.nick@yandex.ru>
2023-03-12 13:30:46 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -9,6 +9,8 @@ public class UsageMap {
         HashMap<String, String> hm = new HashMap<>();
         hm.put("123@mail.ru", "Иванов Иван Иванович");
         hm.put("1234@mail.ru", "Новиков Олег Олегович");
+        hm.put("1234@mail.ru", "Новиков Олег Олегович");
+        hm.put("1234@mail.ru", "Новиков Олег Олегович");
         hm.put("12@mail.ru", "Илюхин Артем Андреевич");
         for (Map.Entry<String, String> entry : hm.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
