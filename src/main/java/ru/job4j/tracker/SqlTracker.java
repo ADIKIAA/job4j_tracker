5754f82d98104e469b465767209d3907252a5236
ADIKIAA <zhekov.nick@yandex.ru>
2023-06-12 13:53:09 +0300

    1. Liquibase. Установка. [#504892]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import org.postgresql.util.PSQLState;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
@@ -18,8 +19,7 @@ public class SqlTracker implements Store {
     }
 
     private void init() {
-        try (InputStream in = SqlTracker.class.getClassLoader()
-                .getResourceAsStream("app.properties")) {
+        try (InputStream in = new FileInputStream("./db/liquibase.properties")) {
             Properties config = new Properties();
             config.load(in);
             Class.forName(config.getProperty("driver-class-name"));
